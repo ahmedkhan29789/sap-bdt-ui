c9cc7cfbca2526edbc4e4abcb252b777850ee993
ahmedkhan29789 <ahmedkhan29789@gmail.com>
2021-11-18 16:53:02 +0100

    Update ProcessFlow.controller.js

--- app/test-resources/ext/controller/ProcessFlow.controller.js
+++ app/test-resources/ext/controller/ProcessFlow.controller.js
@@ -1,5 +1,5 @@
 //the UI5_184 view controller pair has to be used for UI5 version >= 1.84
-sap.ui.define([
+sap.ui.define([ 
 	'sap/ui/core/mvc/Controller',
 	'sap/fe/core/controllerextensions/RoutingListener',
 	'sap/ui/model/json/JSONModel',
